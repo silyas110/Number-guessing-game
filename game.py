5991928bcc4479264a03de4f5fb54e1c83d198ac
Syed Ilyas Hussain <s.ilyas110@hotmail.com>
2025-10-19 14:09:43 -0400

    Add attempts counter

--- game.py
+++ game.py
@@ -7,11 +7,11 @@ print("I'm thinking of a number between 1 and 10")
 # Computer picks a random number
 secret_number = random.randint(1, 10)
 guess = None
-
+attempts = 0
 # Game loop
 while guess != secret_number:
     guess = int(input("Enter your guess: "))
-    
+    attempts += 1
     if guess < secret_number:
         print("📈 Too low! Try again.")
     elif guess > secret_number:
